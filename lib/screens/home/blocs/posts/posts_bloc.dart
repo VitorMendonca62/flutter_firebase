2b395e8789f5d50f81c0627af467a1157eecc987
VitorMendonca62 <vitorqueiroz325@gmail.com>
2025-04-16 21:39:42 -0300

    remove: removendo imports desnecessarios

--- lib/screens/home/blocs/posts/posts_bloc.dart
+++ lib/screens/home/blocs/posts/posts_bloc.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 import '../../repositories/posts_repository.dart';
 import 'package:flutter_firebase/models/post/post_model.dart';
 
--- lib/screens/home/pages/container_page.dart
+++ lib/screens/home/pages/container_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/screens/home/pages/subpages/home_page.dart';
-import 'package:flutter_firebase/screens/home/widgets/post.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ContainerPage extends StatefulWidget {
