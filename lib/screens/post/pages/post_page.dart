d481c59c3af319429aec67c93994e74f09263781
VitorMendonca62 <vitorqueiroz325@gmail.com>
2025-04-22 18:08:41 -0300

    remove: removendo import desnecessario

--- lib/screens/post/pages/post_page.dart
+++ lib/screens/post/pages/post_page.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_firebase/models/comment/comment_model.dart';
 import 'package:flutter_firebase/models/post/post_model.dart';
 import 'package:flutter_firebase/screens/galery_page.dart';
 import 'package:flutter_firebase/screens/post/bloc/post/post_bloc.dart';
